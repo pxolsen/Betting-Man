96fa393989a1c2ee5f31e087c837e206d54643c5
peter_olsen <peterxavierolsen@gmail.com>
2023-08-17 21:10:35 -0400

    Fixed Navbar v2

--- front_end/betting-man/src/components/Navbar.jsx
+++ front_end/betting-man/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar(props) {
             user ?
             <>
             <Link to="bets">My Bets</Link>
-            <Link to="home"><h1 className="text-3xl font-bold">BETTING MAN</h1></Link>
+            <Link to="/"><h1 className="text-3xl font-bold">BETTING MAN</h1></Link>
             <Link to="profile">Profile</Link>
             {/* <Link onClick={logOut}>Logout</Link> */}
             </>
